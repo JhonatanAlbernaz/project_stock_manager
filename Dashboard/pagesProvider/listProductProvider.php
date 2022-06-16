0da22ab6e4266d02cec4dbd9025b8174da9830cd
JhonatanAlbernaz <jhonatanalbernaz124@gmail.com>
2022-06-16 17:26:21 -0300

    Style

--- Dashboard/pagesProvider/listProductProvider.php
+++ Dashboard/pagesProvider/listProductProvider.php
@@ -29,21 +29,12 @@
   <!-- inject:css -->
   <link rel="stylesheet" href="../css/vertical-layout-light/style.css">
   <link rel="stylesheet" href="../../assets/css/app.css">
-  <link rel="stylesheet" href="../css/app.css">
+  <link rel="stylesheet" href="../css/style.css">
   <!-- endinject -->
   <link href="../images/favicon.ico" rel="icon">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.js"></script>
-
-  <style>
-    .link-menu {
-      margin-left: 50px;
-      margin-top: -30px;
-      position: absolute;
-      text-decoration: none !important;
-    }
-  </style>
 
 </head>
 <body>
--- Dashboard/pagesProvider/listSupply.php
+++ Dashboard/pagesProvider/listSupply.php
@@ -30,70 +30,12 @@
   <!-- inject:css -->
   <link rel="stylesheet" href="../css/vertical-layout-light/style.css">
   <link rel="stylesheet" href="../../assets/css/app.css">
-  <link rel="stylesheet" href="../css/app.css">
+  <link rel="stylesheet" href="../css/style.css">
   <!-- endinject -->
   <link href="../images/favicon.ico" rel="icon">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Counter-Up/1.0.0/jquery.counterup.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.js"></script>
-
-  <style>
-
-    .margin-left-210px {
-        margin-left: 10px;
-    }
-
-    .title-list-provider {
-        margin-left: 285px !important;
-    }
-
-    .divprovider img {
-        width: 75px;
-        margin-top: -15px;
-        border-top-left-radius: 10px;
-        border-bottom-left-radius: 10px;
-    }  
-
-    .div-X {
-      width: 9%;
-      height: 75px;
-      margin-left: 765px;
-      margin-top: -75px;
-      text-align: center;
-      box-shadow: 0 0 24px 0 rgb(0 0 0 / 12%);
-      padding: 25px;
-      font-size: 25px;
-    }
-
-    .div-X i {
-      color: red;
-    }
-
-    .div-check {
-      width: 9%;
-      height: 75px;
-      border-top-right-radius: 10px;
-      margin-left: 865px;
-      border-bottom-right-radius: 10px;
-      box-shadow: 0 0 24px 0 rgb(0 0 0 / 12%);
-      margin-top: -75px;
-      text-align: center;
-      padding: 25px;
-      font-size: 25px;
-    }
-
-    .div-check i {
-      color: green;
-    }
-
-    .link-menu {
-      margin-left: 50px;
-      margin-top: -30px;
-      position: absolute;
-      text-decoration: none !important;
-    }
-
-  </style>    
 
 </head>
 <body>
